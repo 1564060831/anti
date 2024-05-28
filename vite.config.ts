60a2f2ed98eabec0fd9688856e55e8d56c05783c
caiwenxian <996592084@qq.com>
2024-05-28 16:44:19 +0800

    更改打包输出文件夹

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:'/anti.github.io/'
+  base:'/anti.github.io/',
+  build: {
+    outDir: 'docs' // 将打包输出到docs文件夹
+  }
 })
